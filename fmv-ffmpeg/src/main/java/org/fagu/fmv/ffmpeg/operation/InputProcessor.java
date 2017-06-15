40307fb09e7de4d4f7dd84cac9eb55ec6e6c5a1e
f.agu <fagu@oodrive.fr>
2017-06-15 09:59:08 +0200

    add -re

--- fmv-ffmpeg/src/main/java/org/fagu/fmv/ffmpeg/operation/InputProcessor.java
+++ fmv-ffmpeg/src/main/java/org/fagu/fmv/ffmpeg/operation/InputProcessor.java
@@ -113,6 +113,19 @@ public class InputProcessor extends Processor<InputProcessor> implements FilterI
 	}
 
 	/**
+	 * Read input at native frame rate. Mainly used to simulate a grab device, or live input stream (e.g. when reading
+	 * from a file). Should not be used with actual grab devices or live input streams (where it can cause packet loss).
+	 * By default ffmpeg attempts to read the input(s) as fast as possible. This option will slow down the reading of
+	 * the input(s) to the native frame rate of the input(s). It is useful for real-time output (e.g. live streaming).
+	 * 
+	 * @return
+	 */
+	public InputProcessor readRealTime() {
+		add(Parameter.before(ioEntity, "-re"));
+		return this;
+	}
+
+	/**
 	 * @see org.fagu.fmv.ffmpeg.operation.Processor#codec(org.fagu.fmv.ffmpeg.operation.Type, java.lang.String)
 	 */
 	@Override
