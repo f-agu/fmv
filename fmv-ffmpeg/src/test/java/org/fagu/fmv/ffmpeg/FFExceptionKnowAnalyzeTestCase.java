9f36d79c2c8cc7d684aaa77fbf5f2fb90ade1cd5
Fabrice Agu <f.agu@oodrive.com>
2018-05-02 15:16:02 +0200

    test u

--- fmv-ffmpeg/src/test/java/org/fagu/fmv/ffmpeg/FFExceptionKnowAnalyzeTestCase.java
+++ fmv-ffmpeg/src/test/java/org/fagu/fmv/ffmpeg/FFExceptionKnowAnalyzeTestCase.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.fagu.fmv.ffmpeg.metadatas.MovieMetadatas;
+import org.fagu.fmv.soft.exec.CommandLineUtils;
 import org.fagu.fmv.soft.exec.exception.FMVExecuteException;
 import org.junit.Test;
 
@@ -68,8 +69,8 @@ public class FFExceptionKnowAnalyzeTestCase {
 			File file = resource != null ? ResourceUtils.extract(resource, folder) : folder;
 			try {
 				MovieMetadatas extract = MovieMetadatas.with(file)
-						// .customizeExecutor(e -> e.debug())
-						// .customizeExecutor(e -> System.out.println(CommandLineUtils.toLine(e.getCommandLine())))
+						.customizeExecutor(e -> e.debug())
+						.customizeExecutor(e -> System.out.println(CommandLineUtils.toLine(e.getCommandLine())))
 						.extract();
 				fail(expectedMessage + ": " + extract.toJSON());
 			} catch(FMVExecuteException e) {
