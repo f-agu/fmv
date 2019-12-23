365050e2d674d013b12395b9f09a6687030ac0a3
Fabrice Agu <f.agu@oodrive.com>
2019-12-23 16:06:34 +0100

    spring boot: 2.1.5 -> 2.2.2

--- fmv-soft-spring-boot-2.x/src/main/java/org/fagu/fmv/soft/spring/actuator/SoftFoundHealthIndicator.java
+++ fmv-soft-spring-boot-2.x/src/main/java/org/fagu/fmv/soft/spring/actuator/SoftFoundHealthIndicator.java
@@ -39,9 +39,6 @@ public class SoftFoundHealthIndicator extends AbstractHealthIndicator {
 		super("soft check failed");
 	}
 
-	/**
-	 * @see org.springframework.boot.actuate.health.AbstractHealthIndicator#doHealthCheck(org.springframework.boot.actuate.health.Health.Builder)
-	 */
 	@Override
 	protected void doHealthCheck(Builder builder) throws Exception {
 		Collection<Soft> softs = Softs.getHealthIndicators();
