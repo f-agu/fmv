20db7901dafff8720b3fc73029e5b867cc38e7a7
Fabrice Agu <f.agu@oodrive.com>
2022-01-11 11:26:47 +0100

    pom version

--- deploy-with-curl.bat
+++ deploy-with-curl.bat
@@ -2,7 +2,7 @@
 
 rem set PWD=%~dp0
 rem set PWD=D:\tmp\fmv\fmv\
-set VERSION=0.24.0.10-SNAPSHOT
+set VERSION=0.24.0.11-SNAPSHOT
 rem set VERSION=0.18.4
 
 set REPO_URL=https://nexus.oodrive.net/nexus/
